1d0cf4946f2b2b7f8992e527354a05042c008db9
robof123 <jbhm04@gmail.com>
2024-11-10 21:04:16 +0100

    Update and rename script2.js to script.js

--- Logowanie/script.js
+++ Logowanie/script.js
@@ -1,19 +1,22 @@
 const logoutButton = document.getElementById('logout');
+const lobbyButton = document.getElementById('lobbyButton');  // Dodanie referencji do przycisku "Wróć do lobby"
 
 // Sprawdzenie, czy użytkownik jest zalogowany
 if (localStorage.getItem("isAuthenticated") !== "true") {
     // Jeśli nie, przekierowanie z powrotem na stronę logowania
-    window.location.href = "index.html";
+    window.location.href = "../index.html";
 } else {
     // Ustawienie timera do automatycznego wylogowania po 10 sekundach
     setTimeout(() => {
         localStorage.removeItem("isAuthenticated");
-        window.location.href = "index.html";
+        window.location.href = "../index.html";
     }, 10000); // 10 sekund
 }
 
 // Obsługa wylogowania po kliknięciu przycisku
 logoutButton.addEventListener('click', () => {
     localStorage.removeItem("isAuthenticated");
-    window.location.href = "index.html";
+    window.location.href = "../index.html";
 });
+
+
